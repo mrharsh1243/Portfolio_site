93e1418e0428af9f250fd3eb1f7dd497fbd7739d
harsh <hp12442133@gmail.com>
2023-12-11 18:52:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// // toggle icon navbar
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+
+// // scroll sections
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.screenY > 100);
+
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+
+    // animation footer on scroll
+
+}
